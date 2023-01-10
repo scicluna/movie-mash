30ceedbc241a634e7f397e256c62359b0d8e2966
scicluna <88948700+scicluna@users.noreply.github.com>
2023-01-10 14:08:06 -0500

    Added OMDB functionality and DOM transformations

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,12 @@
 // Fetch based on search data -> search data will give us the name of the movie -> search omdb for specific reviews && wikipedia for exerp
 //Select DOMS
 const searchInput = document.querySelector(".searchbar")
-const searchBtn = document.querySelector(".searchbtn")
+const searchBtn = document.querySelector(".button")
+const moviePoster = document.querySelector(".moviePoster")
+const movetitle = document.querySelector(".movieTitle")
+const movieDesc = document.querySelector(".movieDesc")
+const wikiDesc = document.querySelector(".wikiDesc")
+const movieRating = document.querySelector(".movieRating")
 
 //Init Button
 searchBtn.addEventListener("click", movieSearch)
@@ -26,7 +31,6 @@ fetch(requestUrl)
   .then(function (data) {
     console.log(data);
     findRecentTitle(data)
-
   });
 
 
@@ -48,11 +52,11 @@ function findRecentTitle(data){
     }
 
     //Passing in our most recent title and the IMBD id into our cascading fetch function
-    fetchRecentTitle(mostRecentTitle, imbdID)
+    fetchRecentTitle(mostRecentTitle, imbdID, mostRecentYear)
 }
 
 //Use the data we get from OMBD to search up more information on the movie
-function fetchRecentTitle(title, id){
+function fetchRecentTitle(title, id, date){
     //Use the IMBD ID to more specifically look up the movie on OMBD
     let recentUrl = `http://www.omdbapi.com/?i=${id}&apikey=83e0357b`
 
@@ -61,16 +65,16 @@ function fetchRecentTitle(title, id){
     return response.json();
   })
   .then(function (data) {
-    //do things with the data here
-    //Display poster
-    //Movie ratings
-    //Movie Summary
-    //Run time
-    //etc... this object has like everything about the movie
     console.log(data);
+    //do things with the data here
+    //etc... this object has like everything about the movie
+    ombdGenerate(data)
+
   });
 
-    //Our API search for wikipedia using the movie title
+    //Our API search for wikipedia using the movie title//Add url to wiki search
+    //need wiki search url in addition that lets us query wikipedia and search for the film
+    //let requestUrl =  "http://en.wikipedia.org/w/api.php?action=opensearch&search=Hulk&format=json&origin=*"
     let wikiUrl = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${title}&origin=*`
 
     fetch(wikiUrl)
@@ -81,6 +85,7 @@ function fetchRecentTitle(title, id){
     //do things with the data here
     console.log(data);
     //gives us a wikipedia extract
+    //build wikipedia algorithm that searches out the film tag and prefers that if available
     let wikiID = Object.getOwnPropertyNames(data.query.pages)[0]
     console.log(data.query.pages[wikiID].extract)
     //sketchy, but can be used as a "click for more" type button
@@ -91,4 +96,17 @@ function fetchRecentTitle(title, id){
 
   //Insert youtube stuff here
 
+}
+
+function ombdGenerate(data){
+  moviePoster.src = data.Poster
+  movieDesc.innerText = data.Plot
+
+  for (let i=0; i<data.Ratings.length; i++){
+    let newLi = document.createElement("li")
+    let newLiContent = `<b>${data.Ratings[i].Source}</b> ${data.Ratings[i].Value}`
+    newLi.innerHTML = newLiContent
+    movieRating.appendChild(newLi)
+  }
+  
 }
